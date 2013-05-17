7346a1d46b7406ca8d9e52e04c94731903de5860
Jason Zopf <jasonzopf@gmail.com>
2013-05-17 12:56:49 -0400

    more broken windows

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,13 @@
 class Activity < ActiveRecord::Base
-  attr_accessible :description
+  attr_accessible :description, :created_at, :name #remove name
 
   validates_length_of :description, minimum: 10, message: "Give me moar!"
+
+  def self.old_activities
+    where({})
+  end
+
+  def self.reverse_alphabetical_order
+    order({})
+  end
 end
--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Activity do
  
  it "requires a description field" do
-  activity = Activity.create(name: "big catch")
+  activity = Activity.create(name: "big catch woo woo")
   expect(activity).to be_invalid
   activity.description = "fishing trip"
   expect(activity).to be_valid
@@ -25,7 +25,23 @@ describe Activity do
 
  it "returns the activity name in ALL CAPS" do
   name = "pizza party"
-  activity = Activity.create({name: name, description: "fun times"})
+  activity = Activity.create({name: name, description: "fun times woo woo"})
   expect(activity.loud_name).to eql(name.upcase)
+ end
+
+ it "returns old activities" do
+  old_activity1 = Activity.create({created_at: Time.now - 3.years, name: "A name", description: "fun times woo woo"})
+  old_activity2 = Activity.create({created_at: Time.now - 2.years, name: "A name", description: "fun times woo woo"})
+  new_activity = Activity.create({name: "L name", description: "fun times woo woo"})
+  expect(Activity.old_activities).to include old_activity1
+  expect(Activity.old_activities.length).to eql(2)
+  expect(Activity.old_activities).not_to include new_activity
+ end
+
+ it "returns the activities in reverse alphabetical order by name" do
+  activity1 = Activity.create({name: "A name", description: "fun times woo woo"})
+  activity2 = Activity.create({name: "Z name", description: "fun times woo woo"})
+  activity3 = Activity.create({name: "L name", description: "fun times woo woo"})
+  expect(Activity.reverse_alphabetical_order.first).to eql(activity2)
  end
 end
