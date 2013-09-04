005a5d81989e3d3aed5fa0957d33cac4905f4b48
Louise Au Yeung <louise@eabx.org>
2013-09-04 18:41:20 -0400

    all tests are passing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130517034623) do
+ActiveRecord::Schema.define(:version => 20130904211107) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20130517034623) do
   create_table "votes", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "note"
   end
 
 end
